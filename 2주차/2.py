1a1123a2a2985d76e6cb931e052ea8df06083217
USER\User <D502>
2024-03-18 15:10:44 +0900

    2주차 (변수,연산자)

---
+++ 2주차/2.py
@@ -0,0 +1,33 @@
+# num = int(input("정수를 입력하세요 : "))
+# if num % 2 == 0:
+#     print(num, '은(는) 짝수입니다.')
+# else:
+#     print(num, '은(는) 홀수입니다.')
+
+# x = int(input("정수를 입력하세요 : "))
+# if x < 0: #0보다 x가 작을때
+#     print("x = ", x)
+#     print(x, "은(는) 음수입니다.")
+# else: #아닐 경우
+#     print("x = ", x) 
+#     print(x, "은(는) 양수입니다.")
+
+# game_score = int(input("게임점수를 입력하세요 : "))
+# if game_score <= 1000:
+#     print("입문자 입니다.")
+# elif game_score > 1000:
+#     print("고수입니다")
+
+# #윤년 판별?
+# year = int(input("연도를 입력하세요 : "))
+# is_leap_year = ((year % 4 == 0) and (year % 100 != 0) or year % 400 == 0)
+# #4와 100으로 나눴을때 0이 되거나 또는 400으로 나눴을때 0이 되는
+# print(year, '년은 윤년입니까? ', is_leap_year)
+# #year , 질문, 23번줄에서 판별
+
+# 첫번째 두번째 입력받은 숫자 사이값
+# num1 = int(input("첫번째 숫자를 입력하세요 : "))
+# num2 = int(input("두번째 숫자를 입력하세요 : "))
+# for i in range(num1, num2):
+#     print(i, end = ' ')
+
